d25fd2e051adf8a47fb97db945fe7bb457ba5c28
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-11-14 09:54:00 -0600

    Test

---
+++ test.py
@@ -0,0 +1,23 @@
+def bubble_sort(arr):
+    temp = 0
+    num_swaps = 1
+    while num_swaps > 0:
+        num_swaps = 0
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                num_swaps += 1
+    return arr   
+
+def main():
+    arr1 = [3, 9, 1, 2, 10, 7, 5, 4, 8, 6]
+    print(arr1)
+    print(bubble_sort(arr1))
+    print("Enter your name: ")
+    name = input()
+    print("Hello, " + name + ". Welcome.")
+
+if __name__ == "__main__":
+    main()
